87d4870267086479ad78be862edd60b17406f4d2
Krollbotid <krollbotid@gmail.com>
2022-09-24 21:44:14 +0300

    Fixed typo in the name of directory. Lab2: Class ready to be tested.

---
+++ Term 3. Object-Oriented Programming/lab2/ButaLib/buta.cpp
@@ -0,0 +1,106 @@
+//
+// Created by USER on 24.09.2022.
+//
+
+#include "buta.h"
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+
+using namespace butas;
+
+Buta::Buta(double m, double c) : p(0, 0), m(m), c(c) {}
+Buta::Buta(const Point &p0, double m, double c): p(p0), m(m), c(c) {}
+Buta::Buta(double x0, double y0, double m, double c): p(x0, y0), m(m), c(c) {}
+
+Buta& Buta::setP(const Point &p0){ p = p0; return *this; }
+Buta& Buta::setM(double M){ m = M; return *this; }
+Buta& Buta::setC(double C){ c = C; return *this; }
+
+Point Buta::getP() const{ return p; }
+double Buta::getM() const{ return m; }
+double Buta::getC() const{ return c; }
+
+types Buta::getType() const {
+    if (c == 0) {
+        return types::BernoulliLemniscate;
+    }
+    if (c > 2 * m * m) {
+        return types::ellipticPedal;
+    }
+    if (c == 2 * m * m) {
+        return types::Circles;
+    } else {
+        return types::hyperbolicPedal;
+    }
+}
+
+const char* Buta::getPrintableType() const {
+    const char* arr[] = {
+            "Bernoulli Lemniscate",
+            "Elliptic pedal curve",
+            "2 circles",
+            "Hyperbolic pedal curve"
+    };
+    return arr[this->getType()];
+}
+
+double Buta::area() const {
+    if (c == 0) {
+        return 2 * m * m;
+    }
+    if (c > 2 * m * m) {
+        return PI * c;
+    }
+    if (c == 2 * m * m) {
+        return PI * m * m * 2;
+    } else {
+        double a = 2 * m * m + c, b = a - c - c;
+        return c * atan(a / b) + a * b / 2;
+    }
+}
+
+double Buta::getFirstCoefficient() const {
+    if (c == 0) {
+        return sqrt(2 * m * m);
+    }
+    if (c != 2 * m * m) {
+        return sqrt(2 * m * m + c);
+    } else {
+        return 2 * abs(m);
+    }
+}
+
+double Buta::getSecondCoefficient() const {
+    if (c == 0) {
+        return sqrt(2 * m * m);
+    }
+    if (c > 2 * m * m) {
+        return sqrt(2 * m * m - c);
+    } else {
+        return 0;
+    }
+}
+
+char* Buta::getPrintableEquation() const{
+    double k1 = this->getFirstCoefficient(), k2 = this->getSecondCoefficient();
+    const char* s1 = "r ^ 2 = ( * cos(phi)) ^ 2\n", *s2 = " - ( * sin(phi)) ^ 2";
+    size_t l = strlen(s1) + 1, k;
+    if (k2 != 0) {
+        l += strlen(s2);
+    }
+    char num[20];
+    sprintf(num, "%.2f", k1);
+    l += strlen(num);
+    sprintf(num, "%.2f", k2);
+    l += strlen(num);
+    char *s = new char[l];
+    sprintf(s, "r ^ 2 = (%.2f * cos(phi)) ^ 2", k1);
+    if (k2 != 0) {
+        k = strlen(s);
+        sprintf(s + k, " - (%.2f * sin(phi)) ^ 2", k2);
+    }
+    k = strlen(s);
+    sprintf(s + k, "\n");
+    return s;
+}
---
+++ Term 3. Object-Oriented Programming/lab2/ButaLib/buta.h
@@ -0,0 +1,48 @@
+//
+// Created by USER on 24.09.2022.
+//
+
+#ifndef LAB2_BUTA_H
+#define LAB2_BUTA_H
+#define PI 3.14159
+
+namespace butas {
+    enum types {
+        ellipticPedal = 0,
+        hyperbolicPedal,
+        Circles,
+        BernoulliLemniscate
+    };
+
+    struct Point {
+        double x, y;
+        Point(double x0 = 0, double y0 = 0) : x(x0), y(y0) {}
+    };
+
+    class Buta { // (x^2+y^2)^2-(2m^2+c)*x^2+(2m^2-c)*y^2=0
+    private:
+        Point p; // center
+        double m, c; // parameters of curve
+    public:
+        Buta(double m = 1, double c = 1);
+        Buta(const Point &p0, double m = 1, double c = 1);
+        Buta(double x0, double y0, double m = 1, double c = 1);
+
+        Buta& setP(const Point &p0);
+        Buta& setM(double M);
+        Buta& setC(double C);
+
+        Point getP() const;
+        double getM() const;
+        double getC() const;
+
+        types getType() const;
+        const char* getPrintableType() const;
+        double area() const;
+        double getFirstCoefficient() const; // returns a from r^2 = a^2 * (cos(phi))^2 - b^2 * (sin(phi))^2
+        double getSecondCoefficient() const; // returns b from r^2 = a^2 * (cos(phi))^2 - b^2 * (sin(phi))^2
+        char* getPrintableEquation() const;
+    };
+}
+
+#endif //LAB2_BUTA_H
